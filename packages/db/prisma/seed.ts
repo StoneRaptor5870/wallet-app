7c9861931b1ad704396257a601c026456e582f52
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-31 01:43:58 +0530

    on ramp txns

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -8,7 +8,7 @@ async function main() {
     update: {},
     create: {
       number: "1111111111",
-      password: await bcrypt.hash("alice", 10),
+      password: await bcrypt.hash("12345678", 10),
       name: "alice",
       Balance: {
         create: {
@@ -32,7 +32,7 @@ async function main() {
     update: {},
     create: {
       number: "2222222222",
-      password: await bcrypt.hash("bob", 10),
+      password: await bcrypt.hash("12345678", 10),
       name: "bob",
       Balance: {
         create: {
