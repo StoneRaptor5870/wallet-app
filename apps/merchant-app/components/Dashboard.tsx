1d0e9cc969b4843d77112df23f59d1078e683b8d
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-06-13 02:10:27 +0530

    merchant time series graph

--- apps/merchant-app/components/Dashboard.tsx
+++ apps/merchant-app/components/Dashboard.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { Card } from "@repo/ui/card";
+import LineChart from "./TimeSeries";
 
-export default function Home() {
+interface DashboardProps {
+  amounts: number[];
+  times: string[];
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ amounts, times }) => {
   return (
-    <div className="flex justify-center items-center w-full h-full">
+    <div className="flex flex-col justify-center items-center w-full h-full">
       <Card title="Home">
+      <div className="flex flex-col justify-center items-center w-full h-full gap-4 mb-8">
+        {/* @ts-ignore */}
+        <LineChart amounts={amounts} times={times} />
+        <div className="flex justify-center items-center p-4">This time series graph tells about the peer to peer transactions made by the user. 
+          Amount inflow and outflow, you can change the time axis with given buttons too.</div>
+      </div>
         <div className="flex flex-col justify-center items-center">
           <div className="w-full p-4">
             <p className="text-xl">Bank to Wallet Transfer</p>
@@ -19,13 +31,13 @@ export default function Home() {
           <div className="w-full p-4">
             <p className="text-xl">Peer to Peer Transfer</p>
             <div>
-              This feature lets you transfer money to another user by their email.
+              This feature lets you transfer money to another user by their phone number.
             </div>
           </div>
           <div className="w-full p-4">
-            <p className="text-xl">Merchant to User Transfer</p>
+            <p className="text-xl">User to Merchant Transfer</p>
             <div>
-              This feature lets you transfer money to an end-user by their phone number.
+              This feature lets you transfer money to merchant by their email.
             </div>
           </div>
         </div>
@@ -33,3 +45,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Dashboard;
