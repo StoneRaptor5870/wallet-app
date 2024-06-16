ea3a24dc7dd20f3140692a9d5ffe04fd3af1508b
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-06-16 21:58:54 +0530

    fix

--- apps/bank-webhook-server/app/api/tokenGenerator/route.ts
+++ apps/bank-webhook-server/app/api/tokenGenerator/route.ts
@@ -1,19 +1,26 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import { config } from 'dotenv';
 
 config();
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
   try {
+    // Parse the URL to get the query parameters
+    const url = new URL(req.url);
+    const params = new URLSearchParams(url.search);
+    const nonce = params.get('nonce');
+
+    console.log('Received nonce:', nonce);
+
     const transactionId = uuidv4();
     const payload = { transactionId };
     const token = jwt.sign(payload, process.env.JWT_SECRET || '', { expiresIn: '1h' });
 
     const response = NextResponse.json({ token });
-    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
     response.headers.set('Pragma', 'no-cache');
     response.headers.set('Expires', '0');
     response.headers.set('Surrogate-Control', 'no-store');
