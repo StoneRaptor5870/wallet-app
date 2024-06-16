554313125c1e48ab07de63bfe9f03988f559c7b1
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-06-16 21:44:23 +0530

    fix

--- apps/bank-webhook-server/app/api/tokenGenerator/route.ts
+++ apps/bank-webhook-server/app/api/tokenGenerator/route.ts
@@ -6,14 +6,21 @@ import { config } from 'dotenv';
 
 config();
 
-export const GET = async() => {
+export const GET = async () => {
   try {
     const transactionId = uuidv4();
     const payload = { transactionId };
     const token = jwt.sign(payload, process.env.JWT_SECRET || '', { expiresIn: '1h' });
-    return NextResponse.json({ token });
+
+    const response = NextResponse.json({ token });
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    response.headers.set('Surrogate-Control', 'no-store');
+
+    return response;
   } catch (error) {
     console.error('Error generating token:', error);
-    return NextResponse.json({ message: 'Error generating token' }, {status: 500});
+    return NextResponse.json({ message: 'Error generating token' }, { status: 500 });
   }
 }
